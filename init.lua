1da8552e9072f7842122eeed27cb174a636aef9f
JNSFilipe <jose.filipe@ieee.org>
2024-01-03 21:36:21 +0000

    Fixed movement within Telescope

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 -- TODO:
--- [ ] Fix telescope moving up and down
 -- [ ] DAP
 -- [ ] Nice icons / configure lua line
 -- [ ] Disable cmp before typing starts
@@ -766,18 +765,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- [[ Configure Telescope ]]
--- See `:help telescope` and `:help telescope.setup()`
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
-}
-
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
