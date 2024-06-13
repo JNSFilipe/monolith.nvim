fda256c8d7915db1afa74175cb635a46004482b5
JNSFilipe <jose.filipe@ieee.org>
2024-06-13 03:01:29 +0100

    Added syncing functionality to vemacs

--- vemacs.h
+++ vemacs.h
@@ -26,12 +26,19 @@ int deploy_vemacs() {
   MONO_MKDIR(MONO_RESOLVE_ENV_VARS(VEMACS_CONFIG_PATH));
   snprintf(buffer, sizeof(buffer), "%s/%s", MONO_PWD(), "vemacs/init.el");
   snprintf(buffer2, sizeof(buffer), "%s/%s",
-           MONO_RESOLVE_ENV_VARS(VEMACS_CONFIG_PATH), "/init.el");
+           MONO_RESOLVE_ENV_VARS(VEMACS_CONFIG_PATH), "init.el");
   mono_log(MONO_INFO, "[VEMACS] CREATE SYMLINK TO:");
   mono_log(MONO_INFO, buffer);
   int err = MONO_SYMLINK(buffer, buffer2);
   if (err != 0)
     mono_log(MONO_ERROR, "Failed to create symlink");
+
+#ifdef SYNC
+  mono_log(MONO_INFO, "[VEMACS] SYNCING PACKAGES");
+  snprintf(buffer, sizeof(buffer), "emacs --batch --script %s/init.el",
+           MONO_RESOLVE_ENV_VARS(VEMACS_CONFIG_PATH));
+  MONO_SYS(buffer);
+#endif
 
   return 0;
 }
