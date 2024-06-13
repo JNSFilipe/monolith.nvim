5f1ecf34eb717f5c0ea28e1e31eecf6c61150604
JNSFilipe <jose.filipe@ieee.org>
2024-06-13 03:10:37 +0100

    Defined C-l to accept copilot stuff

--- neovim/init.lua
+++ neovim/init.lua
@@ -168,9 +168,7 @@ require('lazy').setup({
 
       if not opts.mapping then opts.mapping = {} end
       opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
-        if copilot.is_visible() then
-          copilot.accept()
-        elseif cmp.visible() then
+        if cmp.visible() then
           cmp.select_next_item()
         elseif luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
@@ -180,6 +178,10 @@ require('lazy').setup({
           fallback()
         end
       end, { "i", "s" })
+
+      opts.mapping["<C-l>"] = cmp.mapping(function()
+        if copilot.is_visible() then copilot.accept() end
+      end)
 
       opts.mapping["<C-x>"] = cmp.mapping(function()
         if copilot.is_visible() then copilot.next() end
@@ -1130,12 +1132,12 @@ mason_lspconfig.setup_handlers {
 
 -- Auto-open Telescope file picker when Neovim starts with no files
 vim.api.nvim_create_autocmd("VimEnter", {
-    pattern = "*",
-    callback = function()
-        if #vim.api.nvim_list_bufs() == 1 and vim.fn.bufname() == "" and vim.fn.argc() == 0 then
-            vim.defer_fn(function()
-                require('telescope.builtin').find_files()
-            end, 0)
-        end
+  pattern = "*",
+  callback = function()
+    if #vim.api.nvim_list_bufs() == 1 and vim.fn.bufname() == "" and vim.fn.argc() == 0 then
+      vim.defer_fn(function()
+        require('telescope.builtin').find_files()
+      end, 0)
     end
+  end
 })
